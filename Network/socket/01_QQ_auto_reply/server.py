ab5f563861579751ba96e6c90e808c6322c4c40b
XuemingNotCuteAtAll <43215381+XuemingNotCute@users.noreply.github.com>
2019-01-30 16:15:16 +0800

    Update server.py

--- Network/socket/01_QQ_auto_reply/server.py
+++ Network/socket/01_QQ_auto_reply/server.py
@@ -22,10 +22,27 @@ while True:
     data = conn.recv(1024)
     print("received from client；", data)
     conn.send(b"Server has got the msg.")
-"""
 
 # 4. 模拟 QQ 自动回复
 while True:
     data = conn.recv(1024)
     print("received from client；", data)
     conn.send(b"AUTO_REPLY: I have got your msg. Sorry I'm not available now, I will reply you as soon as possible.")
+"""
+
+# 5.服务完一个服务另一个用户，但是同时只能服务一个用户
+while True:
+    conn, address = server.accept()
+    print(conn)
+    print(address)
+
+    while True:
+        try:
+            data = conn.recv(1024)
+            print("received from client；", data)
+            conn.send(b"AUTO_REPLY: I have got your msg. Sorry I'm not available now, I will reply "
+                      b"you as soon as possible.")
+        # 这个错误是 windows 抛出的错误，linux 抛出的是另一个异常
+        except ConnectionResetError as e:
+            print(e)
+            break
